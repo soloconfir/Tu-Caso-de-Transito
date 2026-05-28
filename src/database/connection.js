fa16cfdda0ecac03a5228062a3aea32f4c5e5e00
Juan Pablo Chaverra Cabrera <88263070+soloconfir@users.noreply.github.com>
2026-05-28 01:06:41 +0000

    feat: actualizacion de ssl y pool de conexion en connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -10,6 +10,7 @@ export const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
